9546adefb841a791fdef0cd6d49aafb008824eaf
GabrielCostaSENAI <gabriel.costa@sistemafiep.org.br>
2023-08-01 17:50:35 -0300

    Web Page Test

---
+++ Paginas/main.js
@@ -0,0 +1,17 @@
+function addNreTask(){
+    var lista = document.getElementById("list");
+    var text = document.getElementById("taskname").value;
+
+    if(text.length == 0){
+        alert("Tarefa precisa conter mais que 1 caracter");
+        return;
+    }
+
+    var listItem = document.createElement("li");
+    listItem.className = "list-item";
+
+    const textElement = document.createTextNode(text);
+    listItem.appendChild(textElement);
+    list.appendChild(listItem);
+
+}
